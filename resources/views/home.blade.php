a063317051daa916724a995df1ad38d9cc0da249
sourovahmad <mdsourovahmad19@gmail.com>
2020-06-14 08:09:10 +0600

    alldone

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,110 +38,103 @@
     </tr>
   </thead>
   <tbody>
-      
-    @foreach($data as $all)
-    <tr>
-      <th scope="row">{{$all->id}}</th>
-      <td>{{$all->firstname}}</td>
-      <td>{{$all->lastname}}</td>
-      <td>{{$all->email}}</td>
-    
-     	<td class="text-center"> <a href="{{ route('edit',   $all->id )}} " class="btn btn-primary btn-raised btn-sm">
-				<i class="fa fa-pencil-square-o" aria-hidden="true"></i>
-			</a>
-       <button type="button" class="btn btn-danger btn-sm" id="myBtn">edit</button>
+  <?php $i = 1; ?>
+                        @foreach($data as $level)
+                            <?php $id = $level->id; ?>
+                            <tr class="data-row">
+                                <td class="iteration">{{$i++}}</td>
+                                <td class="  word-break firstname">{{$level->firstname}}</td>
+                                <td class=" word-break lastname ">{{$level->lastname}}</td>
+                                <td class=" word-break email ">{{$level->email}}</td>
+                             
 
-			<form method="POST"  action ="{{route('destroy',  $all->id )}} "  id="delete-form-{{  $all->id }}"style="display:none; " >
-				{{csrf_field() }}
-				{{ method_field("delete") }}
-			</form>
+                             
 
 
+                                <td class="align-middle">
+                                <button type="button" class="btn btn-success" id="level-edit-item" data-item-id={{$id}}> <i class="fa fa-edit" aria-hidden="false"> </i></button>
 
-			<button   onclick="if(confirm('are you sure to delete this')){
-				document.getElementById('delete-form-{{  $all->id }}').submit();
+                                    <form method="POST" action="{{ route('destroy',  $level->id )}} " id="delete-form-{{ $level->id }}" style="display:none; ">
+                                        {{csrf_field() }}
+                                        {{ method_field("delete") }}
+                                    </form>
 
-			}
-			else{
-				event.preventDefault();
 
-			}
+                                    <button onclick="if(confirm('are you sure to delete this')){
+                                        document.getElementById('delete-form-{{ $level->id }}').submit();
+                                        }
+                                        else{
+                                        event.preventDefault();
+                                        }
+                                        " class="btn btn-danger btn-sm btn-raised">
+                                        <i class="fa fa-trash" aria-hidden="false"></i>
+                                    </button>
+                                </td>
 
-			" class="btn btn-danger btn-sm btn-raised" >
-				<i class="fa fa-trash" aria-hidden="true">
-				
-				</i>
-      </button>
-    
-		</td>
-	</tr>
- 
-      @endforeach
- 
-   
-  </tbody>
-</table>
-{{$data->links()}}
+                            </tr>
+                        @endforeach
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-    </div> 
 
-  
-@endsection
- 
+    </div>
+
+ <!-- Attachment Modal -->
+    <div class="modal fade" id="level-edit-modal" tabindex="-1" role="dialog" aria-labelledby="edit-modal-label" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-dark" id="edit-modal-label ">Edit Level</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body" id="attachment-body-content">
+                    <form id="level-edit-form" class="form-horizontal" method="POST" action="">
+                    @csrf
+                      
+                    <!-- id -->
+                        <div class="form-group">
+                            <label class="col-form-label" for="modal-input-id">Id </label>
+                            <input type="text" name="id" class="form-control" id="modal-input-id" required readonly>
+                        </div>
+                        <!-- /id -->
+                        <!-- name -->
+                        <div class="form-group">
+                            <label class="col-form-label" for="modal-input-firstname">firstname</label>
+                            <input type="text" name="firstname" class="form-control" id="modal-input-firstname" required autofocus>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-form-label" for="modal-input-lastname">lastname</label>
+                            <input type="text" name="lastname" class="form-control" id="modal-input-lastname" required autofocus>
+                        </div>
+                        <!-- /name -->  
+                        <!-- description -->
+                        <div class="form-group">
+                            <label class="col-form-label" for="modal-input-email">email</label>
+                            <input type="text" name="email" class="form-control" id="modal-input-email" required>
+                        </div>
+                       
+                        <div class="form-group">
+                            <input type="submit" value="Submit" class="form-control btn btn-success">
+                        </div>
+                        <!-- /description -->
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    @endsection
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-    <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header" style="padding:35px 50px;">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4><span class="glyphicon glyphicon-lock"></span> edit</h4>
-        </div>
-        <div class="modal-body" style="padding:40px 50px;">
-          <form role="form">
-            <div class="form-group">
-              <label for="usrname"><span class="glyphicon glyphicon-user"></span> firstname</label>
-              <input type="text" class="form-control" id="firstname" placeholder="Enter firstname">
-            </div>
-            <div class="form-group">
-              <label for="usrname"><span class="glyphicon glyphicon-user"></span> lastname </label>
-              <input type="text" class="form-control" id="lastname" placeholder="Enter lastname ">
-            </div>
-            <div class="form-group">
-              <label for="usrname"><span class="glyphicon glyphicon-user"></span> email</label>
-              <input type="text" class="form-control" id="email" placeholder="Enter email">
-            </div>
- 
-            <div class="checkbox">
-              <label><input type="checkbox" value="" checked>Remember me</label>
-            </div>
-              <button type="submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> update </button>
-          </form>
-        </div>
-        <div class="modal-footer">
-          <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
-          
-        </div>
-      </div>
-      
-    </div>
-  </div> 
-</div>
- 
-<script>
-$(document).ready(function(){
-  $("#myBtn").click(function(){
-    $("#myModal").modal();
-  });
-});
-</script>
 
   </body>
 </html>
